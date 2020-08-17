aa74d8075870136a2e9eeda928c23c0c3a421ab6
Jayden Lao Yi Xue <ylao0002@student.monash.edu>
2020-08-18 03:27:02 +1000

    created home page and admin page

---
+++ SmartBinWeb/home/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+# Create your views here.
+
+posts = [
+    {
+        'author': 'Jayden Lao',
+        'title': 'post 1 ',
+        'content': 'first dummy data',
+        'date': '18 Aug 2020'
+    },
+    {
+        'author': 'Adrian A',
+        'title': 'post 2 ',
+        'content': 'second dummy data',
+        'date': '19 Aug 2020'
+    },
+
+]
+
+
+def home(request):
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'home/home.html', context)
